29d50f843e0893ee832a133624b83c689e16a384
hanseokhyeon <sah0322@naver.com>
2020-03-05 17:09:16 +0900

    add log file

--- loader.py
+++ loader.py
@@ -29,9 +29,19 @@ import numpy as np
 import json
 from torch.utils.data import Dataset, DataLoader
 
-logger = logging.getLogger('root')
-FORMAT = "[%(asctime)s %(filename)s:%(lineno)s - %(funcName)s()] %(message)s"
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format=FORMAT)
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter("[%(asctime)s %(filename)s:%(lineno)s - %(funcName)s()] %(message)s")
+
+now = time.gmtime(time.time())
+stream_handler = logging.StreamHandler()
+file_handler = logging.FileHandler('log/{}_{}_{}_{}h_{}m.log'.format(now.tm_year, now.tm_mon, now.tm_mday,
+                                                                   now.tm_hour, now.tm_min))
+
+stream_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
 logger.setLevel(logging.INFO)
 
 PAD = 0
