2cd41108102039cec40f05921fdaf95a1845411c
hanseokhyeon <sah0322@naver.com>
2020-03-05 15:22:32 +0900

    update

--- run_korean.py
+++ run_korean.py
@@ -374,7 +374,7 @@ def main():
     target_path = os.path.join(DATASET_PATH, 'train_label')
     load_targets(target_path)
 
-    train_batch_num, train_dataset_list, valid_dataset = split_dataset(args, wav_paths, script_paths, valid_ratio=0.05)
+    train_batch_num, train_dataset_list, valid_dataset = split_dataset(args, wav_paths[:4000], script_paths[:4000], valid_ratio=0.1)
 
     logger.info('start')
 
