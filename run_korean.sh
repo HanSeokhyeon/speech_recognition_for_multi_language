54fc8a42dfa03d6fbcb6b2b0209b79744466b356
hanseokhyeon <sah0322@naver.com>
2020-03-08 21:09:50 +0900

    update

--- run_korean.sh
+++ run_korean.sh
@@ -2,8 +2,8 @@
 
 export CUDA_VISIBLE_DEVICES="0"
 
-BATCH_SIZE=4
-WORKER_SIZE=1
-MAX_EPOCHS=10
+BATCH_SIZE=322
+WORKER_SIZE=4
+MAX_EPOCHS=100
 
 python ./run_korean.py --batch_size $BATCH_SIZE --workers $WORKER_SIZE --use_attention --max_epochs $MAX_EPOCHS
