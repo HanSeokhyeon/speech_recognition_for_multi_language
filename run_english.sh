6219186146ec4e47dcb7ac46cdb74ca49dad7770
hanseokhyeon <sah0322@naver.com>
2020-03-10 14:11:55 +0900

    update

--- run_english.sh
+++ run_english.sh
@@ -4,6 +4,6 @@ export CUDA_VISIBLE_DEVICES="0"
 
 BATCH_SIZE=32
 WORKER_SIZE=4
-MAX_EPOCHS=100
+MAX_EPOCHS=1000
 
 python ./run_english.py --batch_size $BATCH_SIZE --workers $WORKER_SIZE --use_attention --max_epochs $MAX_EPOCHS
--- run_korean.sh
+++ run_korean.sh
@@ -2,8 +2,8 @@
 
 export CUDA_VISIBLE_DEVICES="0"
 
-BATCH_SIZE=8
+BATCH_SIZE=16
 WORKER_SIZE=4
-MAX_EPOCHS=100
+MAX_EPOCHS=1000
 
 python ./run_korean.py --batch_size $BATCH_SIZE --workers $WORKER_SIZE --use_attention --max_epochs $MAX_EPOCHS --bidirectional
